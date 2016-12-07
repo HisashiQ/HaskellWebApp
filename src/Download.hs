4200e7b5522332de3ee4da0e315f940a9c0f7e1e
HisashiQ <quinnkoike@gmail.com>
2016-12-07 14:19:21 +0000

    Basic Api consumption

--- src/Download.hs
+++ src/Download.hs
@@ -1,29 +1,54 @@
---This is the "download" file from lab7. As a placeholder to be modified for our own program
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
-module Download (downloadURL) where 
+import Data.Aeson
+import Data.Text
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+import Network.HTTP.Conduit (simpleHttp)
+import GHC.Generics
+import JsonType
 
-import Network.URI
-import Network.HTTP
-import Data.Maybe
-import Data.Either
+-- instance ToJSON Properties
+-- instance FromJSON Geometry
+-- instance ToJSON Geometry
+-- instance FromJSON Coordinates
+-- instance ToJSON Coordinates
+-- -- | Location of the local copy, in case you have it,
+-- --   of the JSON file.
+-- jsonFile :: FilePath
+-- jsonFile = "pizza.json"
 
-{- | Given a valid URI, this function will attempt to download the page contents. -}
-downloadURL :: String -> IO (Either String String)
-downloadURL url = do
-    resp <- simpleHTTP request
-    case resp of
-        Left x -> return $ Left ("Error connecting: " ++ show x)
-        Right r -> case rspCode r of
-                (2,_,_) -> return $ Right (rspBody r)
-                (3,_,_) -> -- A HTTP redirect
-                    case findHeader HdrLocation r of
-                        Nothing -> return $ Left (show r)
-                        Just url -> downloadURL url
-                _ -> return $ Left (show r)
-    where
-        request = Request {
-            rqURI = uri,
-            rqMethod = GET,
-            rqHeaders = [],
-            rqBody = ""}
-        uri = fromJust $ parseURI url
+-- | URL that points to the remote JSON file, in case
+--   you have it.
+jsonURL :: String
+jsonURL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-10-01"
+
+-- Move the right brace (}) from one comment to another
+-- to switch from local to remote.
+
+{--
+-- Read the local copy of the JSON file.
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+--}
+
+{--}
+-- Read the remote copy of the JSON file.
+getJSON :: IO B.ByteString
+getJSON = simpleHttp jsonURL
+--}
+
+main :: IO ()
+main = do
+ -- Get JSON data and decode it
+ -- d <- (eitherDecode <$> getJSON) :: IO (Either String [Earthquake])
+ d <- getJSON
+ -- If d is Left, the JSON was malformed.
+ -- In that case, we report the error.
+ -- Otherwise, we perform the operation of
+ -- our choice. In this case, just print it.
+ -- case d of
+ --  Left err -> putStrLn err
+ --  Right ps -> print "It works"
+ print d
