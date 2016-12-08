1bd6b67f53889b1df40c9fddf31835f4318412ab
HisashiQ <quinnkoike@gmail.com>
2016-12-08 16:03:50 +0000

    rm Holly File

--- Holly/main.hs
+++
@@ -1,7 +0,0 @@
-import DownloadData
-import Parser
-import System.IO
-
-main = do 
-     d <- downloadURL "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-10-01&endtime=2016-12-01"
-     mapM_ putStrLn (getFeatures (head (splitData (fromRight d))))
